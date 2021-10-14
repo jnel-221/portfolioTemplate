3a081fe2c09273903b179765f97099acd394d068
jnelson <jennifer.nelson242@gmail.com>
2021-10-14 13:32:33 -0500

    converted to card

--- portfoliotemplate/src/components/Projects2.js
+++ portfoliotemplate/src/components/Projects2.js
@@ -1,23 +1,31 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 import "../css/componentCSS/Projects2.css";
 
 const Projects2 = () => {
   return (
-    <section className="p-5 m-5" id="projects2">
-      <h4 className="pb-1">
-        Projects2
-      </h4>
-      <img
+    <Card className="p-5 m-5" id="projects2">
+    <Card.Body>
+      <Card.Title className="pb-2">Projects1</Card.Title>
+      <Card.Img
+        variant="left"
         src="https://via.placeholder.com/150"
         alt="placeholder"
         id="left"
-      ></img>
-      <p className="">You have freedom here.</p>
-      <p className="">
-      The only guide is your heart. If we're going to have animals around we all have to be concerned about them and take care of them. You can spend all day playing with mountains. Just let this happen. We just let this flow right out of our minds..
-      </p>
-    </section>
+      />
+      <Card.Text>
+        <p>How do you make a round circle with a square knife?</p>
+        <p>
+          That's your challenge for the day. You can get away with a lot.
+          We'll do another happy little painting. You gotta think like a tree.
+          Water's like me. It's laaazy ... Boy, it always looks for the
+          easiest way to do things Let's make a happy little mountain now.
+        </p>
+      </Card.Text>
+    </Card.Body>
+  </Card>
   );
 };
 
 export default Projects2;
+
--- portfoliotemplate/src/components/projects1.js
+++ portfoliotemplate/src/components/projects1.js
@@ -25,16 +25,8 @@ const Projects1 = () => {
               easiest way to do things Let's make a happy little mountain now.
             </p>
           </Card.Text>
-          {/* <Card.Link href="#">Card Link</Card.Link>
-      <Card.Link href="#">Another Link</Card.Link> */}
         </Card.Body>
       </Card>
-        /* <section className="p-5 m-5" id="projects1">
-            <h4 className="pb-1 project-text" id="title">Projects1</h4>
-            <img src="https://via.placeholder.com/150" alt="placeholder" id="right"></img>
-            <p className="project-text">You can create beautiful things</p> 
-            <p className="project-text">- but you have to see them in your mind first. We'll play with clouds today. This is truly an almighty mountain. Let's build some happy little clouds up here. Let's have a happy little tree in here. I guess that would be considered a UFO. A big cotton ball in the sky. The very fact that you're aware of suffering is enough reason to be overjoyed that you're alive and can experience it. In your imagination you can go anywhere you want. </p>
-        </section> */
     )
 
 }
