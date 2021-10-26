8e7c321258e696688d19980ef07d85f8cf7c2640
jnelson <jennifer.nelson242@gmail.com>
2021-10-26 10:42:01 -0500

    styled video component

--- portfoliotemplate/src/components/projects/VideoProjects/Video.js
+++ portfoliotemplate/src/components/projects/VideoProjects/Video.js
@@ -2,25 +2,22 @@ import React from "react";
 import { Row, Col } from "react-bootstrap";
 import YouTubeEmbed from "./YouTubeEmbed";
 
-function VideoWork(props) {
 
-    let videoData = props.data;
-    
-  console.log("here's what reached VideoWork ", videoData);
+
+function VideoWork(props) {
+  let videoData = props.data;
 
   return (
     <>
-      <div className="video-responsive">
+      <div className="px-3">
         <h2 className="text-center page-title">Video Editing</h2>
         <hr></hr>
-        <Row xs={3} md={6} lg={6}>
-          <Col>
-            {
-                videoData.map((videoID, i) => (
-                <YouTubeEmbed key={i} embedId={videoID.embedid}/>
-              
-            ))}
-          </Col>
+        <Row xs={2} md={3} lg={3}>
+          {videoData.map((videoID, i) => (
+            <Col className="pt-3">
+              <YouTubeEmbed key={i} embedId={videoID.embedid} />
+            </Col>
+          ))}
         </Row>
       </div>
     </>
--- portfoliotemplate/src/components/projects/VideoProjects/YouTubeEmbed.js
+++ portfoliotemplate/src/components/projects/VideoProjects/YouTubeEmbed.js
@@ -1,23 +1,20 @@
 import React from "react";
+import "./video.css"
 
-
-const YouTubeEmbed = ({embedId}) => {
-    console.log("here's what reached YouTubeEmbed",embedId)
-
-    return(
-        <div>
-            <iframe
-            width='560'
-            height='315'
-            src= {`https://www.youtube.com/embed/${embedId}`}
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gryoscope; picture-in-picture"
-            allowFullScreen
-            title="Embedded youtube"
-            />
-        </div>
-    )
-
-}
+const YouTubeEmbed = ({ embedId }) => {
+  return (
+    <div className="p-3 video-responsive">
+      <iframe
+        width="560"
+        height="315"
+        src={`https://www.youtube.com/embed/${embedId}`}
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gryoscope; picture-in-picture"
+        allowFullScreen
+        title="Embedded youtube"
+      />
+    </div>
+  );
+};
 
 export default YouTubeEmbed;
